f5fe0b38a58143591b0c67f8e260e45297d220a2
RoboAIX <roboaix25@gmail.com>
2025-07-09 19:01:20 +0530

    Add files via upload

---
+++ blink_LED_Using_ESP8266.ino
@@ -0,0 +1,30 @@
+#define BLYNK_TEMPLATE_ID "TMPL32s43QfvL"
+#define BLYNK_TEMPLATE_NAME "Blink LED"
+#define BLYNK_AUTH_TOKEN "-gZIyUkmdvfO_pw5uTh7Iu7_ge-5xSaB"
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// Your Wi-Fi credentials
+char ssid[] = "Lalbagh";
+char pass[] = "L646/74pc@H";
+
+#define LED_PIN D2  // Connect LED anode to D2 via resistor
+
+BLYNK_WRITE(V0) {
+  int ledState = param.asInt();
+  digitalWrite(LED_PIN, ledState);
+}
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop() {
+  Blynk.run();
+}
+
+
+
